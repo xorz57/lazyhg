81411f9e79d4d41aa09d5bf9ecbc3c7657e7b795
George Fotopoulos <xorz57@gmail.com>
2024-07-14 15:35:18 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,11 +19,20 @@ fn main() -> io::Result<()> {
 
     let mut should_quit = false;
     let hg_status_output = run_command("hg", &["status"]);
-    let hg_log_output = run_command("hg", &["log"]);
     let hg_branches_output = run_command("hg", &["branches"]);
+    let hg_bookmarks_output = run_command("hg", &["bookmarks"]);
+    let hg_log_output = run_command("hg", &["log"]);
 
     while !should_quit {
-        terminal.draw(|f| ui(f, &hg_status_output, &hg_log_output, &hg_branches_output))?;
+        terminal.draw(|f| {
+            ui(
+                f,
+                &hg_status_output,
+                &hg_branches_output,
+                &hg_bookmarks_output,
+                &hg_log_output,
+            )
+        })?;
         should_quit = handle_events()?;
     }
 
@@ -54,29 +63,43 @@ fn handle_events() -> io::Result<bool> {
     Ok(false)
 }
 
-fn ui(frame: &mut Frame, hg_status_output: &str, hg_log_output: &str, hg_branches_output: &str) {
+fn ui(
+    frame: &mut Frame,
+    hg_status_output: &str,
+    hg_branches_output: &str,
+    hg_bookmarks_output: &str,
+    hg_log_output: &str,
+) {
     let chunks = Layout::default()
-        .direction(Direction::Vertical)
+        .direction(Direction::Horizontal)
         .margin(1)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+        .split(frame.size());
+
+    let left_chunks = Layout::default()
+        .direction(Direction::Vertical)
         .constraints(
             [
-                Constraint::Percentage(70),
+                Constraint::Percentage(60),
                 Constraint::Percentage(20),
-                Constraint::Percentage(10),
+                Constraint::Percentage(20),
             ]
             .as_ref(),
         )
-        .split(frame.size());
+        .split(chunks[0]);
 
     let status_block = Block::bordered().title("Status");
-    let log_block = Block::bordered().title("Log");
     let branches_block = Block::bordered().title("Branches");
+    let bookmarks_block = Block::bordered().title("Bookmarks");
+    let log_block = Block::bordered().title("Log");
 
     let status_paragraph = Paragraph::new(hg_status_output).block(status_block);
-    let log_paragraph = Paragraph::new(hg_log_output).block(log_block);
     let branches_paragraph = Paragraph::new(hg_branches_output).block(branches_block);
+    let bookmarks_paragraph = Paragraph::new(hg_bookmarks_output).block(bookmarks_block);
+    let log_paragraph = Paragraph::new(hg_log_output).block(log_block);
 
-    frame.render_widget(status_paragraph, chunks[0]);
+    frame.render_widget(status_paragraph, left_chunks[0]);
+    frame.render_widget(branches_paragraph, left_chunks[1]);
+    frame.render_widget(bookmarks_paragraph, left_chunks[2]);
     frame.render_widget(log_paragraph, chunks[1]);
-    frame.render_widget(branches_paragraph, chunks[2]);
 }
