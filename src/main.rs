1a7f33ede5d0202aa1f0513838bc9c3e91251467
George Fotopoulos <xorz57@gmail.com>
2024-07-16 00:24:38 +0300

    Remove commands.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self, stdout};
+use std::process::Command;
 
 use ratatui::{
     backend::CrosstermBackend,
@@ -9,25 +10,50 @@ use ratatui::{
     Terminal,
 };
 
-mod commands;
 mod events;
 mod ui;
 
-use commands::run_command;
 use events::handle_events;
 use ui::{draw_ui, ActiveFrame};
 
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
+
     stdout().execute(EnterAlternateScreen)?;
+
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
 
     let mut should_quit = false;
+
     let mut active_frame = ActiveFrame::Status;
-    let hg_status_output = run_command("hg", &["status"]);
-    let hg_branches_output = run_command("hg", &["branches"]);
-    let hg_bookmarks_output = run_command("hg", &["bookmarks"]);
-    let hg_log_output = run_command("hg", &["log"]);
+
+    let hg_status_output = Command::new("hg")
+        .args(&["status"])
+        .output()
+        .expect("Failed to execute command");
+
+    let hg_branches_output = Command::new("hg")
+        .args(&["branches"])
+        .output()
+        .expect("Failed to execute command");
+
+    let hg_bookmarks_output = Command::new("hg")
+        .args(&["bookmarks"])
+        .output()
+        .expect("Failed to execute command");
+
+    let hg_log_output = Command::new("hg")
+        .args(&["log"])
+        .output()
+        .expect("Failed to execute command");
+
+    let hg_status_output = String::from_utf8_lossy(&hg_status_output.stdout).to_string();
+
+    let hg_branches_output = String::from_utf8_lossy(&hg_branches_output.stdout).to_string();
+
+    let hg_bookmarks_output = String::from_utf8_lossy(&hg_bookmarks_output.stdout).to_string();
+
+    let hg_log_output = String::from_utf8_lossy(&hg_log_output.stdout).to_string();
 
     while !should_quit {
         terminal.draw(|f| {
@@ -44,6 +70,8 @@ fn main() -> io::Result<()> {
     }
 
     disable_raw_mode()?;
+
     stdout().execute(LeaveAlternateScreen)?;
+
     Ok(())
 }
